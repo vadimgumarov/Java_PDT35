393cbd995b3bbd41b227b4f83f06d8433788aedb
vadimgumarov <Mishkinis@Mishkiniss-MBP.fios-router.home>
2016-12-02 16:08:20 -0600

    Added some variables to MyFirstProgram

--- sandbox/src/main/java/examples/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/examples/sandbox/MyFirstProgram.java
@@ -2,8 +2,17 @@ package examples.sandbox;
 
 
 public class MyFirstProgram {
+
+
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+
+		String person = "World";
+		System.out.println("Hello," + person);
+
+		int l = 6;
+		int s = l * l;
+		System.out.println("Area of a square with side " + l + " = " + s);
+
 	}
 }
 
