1456317a8f89ac595d6c2ff6ac3eaff6511d915a
vadimgumarov <Mishkinis@Mishkiniss-MBP.fios-router.home>
2016-12-02 16:36:23 -0600

    Added Square and Rectangular functions

--- sandbox/src/main/java/examples/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/examples/sandbox/MyFirstProgram.java
@@ -1,18 +1,39 @@
 package examples.sandbox;
 
-
 public class MyFirstProgram {
-
 
 	public static void main(String[] args) {
 
-		String person = "World";
-		System.out.println("Hello," + person);
+		hello("World");
+		hello("Vadim");
+		hello("User");
 
-		int l = 6;
-		int s = l * l;
-		System.out.println("Area of a square with side " + l + " = " + s);
+		double l = 5;
+		area(4);
+		System.out.println("Area of a square with side " + l + " = " + area(l));
 
+		double a = 4;
+		double b = 6;
+		System.out.println("Area of a rectangular with sides " + a + " and " + b + " = " + area(a, b));
+
+
+	}
+
+
+	public static void hello(String user) {
+
+		System.out.println("Hello, " + user + "!");
+
+	}
+
+	public static double area(double len) {
+		return len * len;
+
+	}
+
+
+	public static double area(double a, double b){
+		return a * b;
 	}
 }
 
